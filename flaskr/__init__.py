c796bd9897d1f697a0ea79cce5f1ce0769804983
Pournami Pradeep <pournamipradeep13@gmail.com>
2026-09-10 21:50:56 +0530

    connect to databse

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,7 +3,6 @@ import os
 from flask import Flask
 
 def create_app(test_config=None):
-    print(__name__,"name")
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
