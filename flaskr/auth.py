7e71ecc9493297560846f521ed8e494c3f9390e6
Pournami Pradeep <pournamipradeep13@gmail.com>
2026-09-16 22:11:08 +0530

    get logged in user function

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, redirect, render_template, request, session, url_for
+from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
 
 from flaskr.db import get_db
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -55,3 +55,13 @@ def login():
             return redirect(url_for('index'))
         flash(error)
     return render_template('auth/login.html')
+
+@bp.before_app_request
+def logged_in_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+            ).fetchone()
